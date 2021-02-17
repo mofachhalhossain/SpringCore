c15eefae9456bda062049f2b35a31467dc67e69d
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-02-17 17:53:02 +0600

    Add files via upload

---
+++ src/main/java/com/springcore/autowire/test.java
@@ -0,0 +1,16 @@
+package com.springcore.autowire;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/autowire/autoconfig.xml");
+		Emp emp1 = context.getBean("emp1", Emp.class);
+		System.out.println(emp1);
+	}
+
+}
